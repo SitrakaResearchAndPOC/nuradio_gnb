b33a27e21418b26d20aeabb62e60c6b270979009
SitrakaResearchAndPOC <92709209+SitrakaResearchAndPOC@users.noreply.github.com>
2026-06-27 10:10:40 +0300

    Add files via upload

---
+++ files/srsan_install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+sudo apt install cmake make gcc g++ pkg-config libfftw3-dev libmbedtls-dev libsctp-dev libyaml-cpp-dev libgtest-dev 
+
+# sudo git clone https://github.com/srsRAN/srsRAN_project
+# Cloner uniquement si le dépôt n'existe pas
+[ ! -d srsRAN_Project ] && git clone https://github.com/srsran/srsRAN_Project.git
+
+cd srsRAN_Project || exit 1
+
+# La dernière version de srsRAN_Projet est seulement readme; pour compiler, toujours avec checkout
+# Latest version : release_25_10
+# sudo git checkout release_25_10
+sudo git checkout 50fe9623c
+
+
+# Créer build uniquement s'il n'existe pas
+[ ! -d build ] && mkdir build
+
+cd build || exit 1
+
+sudo cmake ../
+
+sudo make -j $(nproc --ignore 2)
+
+sudo make test -j $(nproc --ignore 2)
+
+sudo make install
+
+# sudo ldconfig
+
+gnb --version
---
+++ files/uhd_install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+sudo apt update
+ 
+sudo apt install -y git cmake build-essential libboost-all-dev \
+libusb-1.0-0-dev python3-dev python3-mako python3-numpy \ 
+python3-requests python3-setuptools libfftw3-dev libcomedi-dev \ 
+libgps-dev libgmp-dev swig pkg-config gedit
+
+sudo git clone https://github.com/EttusResearch/uhd.git
+
+sudo chown -R $USER:$USER ~/uhd
+
+cd uhd/
+
+sudo git checkout v4.1.0.5
+
+ls
+
+ ( grep -qF 'static const std::regex gp_msg_regex("^\\$G.*$");' host/lib/usrp/gps_ctrl.cpp ||   sed -i '/static const std::regex gp_msg_regex/{s|^|// |;a\
+static const std::regex gp_msg_regex("^\\\\$G.*$");
+}' host/lib/usrp/gps_ctrl.cpp ) && ( grep -qF 'if(msg.substr(1,2) == "GN"){ msg.replace(1, 2, "GP");}' host/lib/usrp/gps_ctrl.cpp ||   sed -i '/msgs\[msg\.substr(1, 5)\] = msg;/i\
+        if(msg.substr(1,2) == "GN"){ msg.replace(1, 2, "GP");}
+' host/lib/usrp/gps_ctrl.cpp )
+
+cd host/
+
+sudo mkdir build
+
+cd build/
+
+sudo cmake .. 
+
+sudo make -j $(nproc --ignore 2)
+
+sudo make install
+
+sudo ldconfig
+
+sudo ln -s /usr/local/lib/uhd/utils/query_gpsdo_sensors /usr/local/bin/query_gpsdo_sensors
+
+sudo uhd_images_downloader 
+
+sudo query_gpsdo_sensors 
