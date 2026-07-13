0289d93f6af0aed088c36974b79f7c2918e3e9eb
SitrakaResearchAndPOC <92709209+SitrakaResearchAndPOC@users.noreply.github.com>
2026-07-13 13:35:58 +0300

    Update open5gs_2.7_install.sh

--- files/open5gs_2.7_install.sh
+++ files/open5gs_2.7_install.sh
@@ -55,6 +55,10 @@ sudo sed -i 's/address: 127.0.0.10/address: 127.0.0.200/' /etc/open5gs/nrf.yaml
 
 # COPY ALL SYSTEMD FILES
 sudo cp /home/nr/open5gs/build/configs/systemd/*.service /etc/systemd/system/
+cd ..
+
+
+
 sudo systemctl daemon-reload
 
 systemctl list-unit-files | grep open5gs
