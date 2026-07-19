af6952076b9581033f7510461af6913f9cfa52f8
SitrakaResearchAndPOC <92709209+SitrakaResearchAndPOC@users.noreply.github.com>
2026-07-19 17:12:15 +0300

    Update install_open5gs_2.7.sh

--- files/install_open5gs_2.7.sh
+++ files/install_open5gs_2.7.sh
@@ -50,7 +50,7 @@ sudo mkdir -p  /var/log/open5gs
 sudo chown -R open5gs:open5gs /var/log/open5gs
 
 # CHANGE THE ADDRESS OF NRF TO PATCH THE CONFIGURATION OF NRF
-sudo sed -i 's/address: 127.0.0.10/address: 127.0.0.200/' /etc/open5gs/nrf.yaml
+# sudo sed -i 's/address: 127.0.0.10/address: 127.0.0.200/' /etc/open5gs/nrf.yaml
 
 # ADD AUTHORIZARION UPFD (upf will not run when you launched service open5gs-webui)
 sudo mkdir -p /etc/systemd/system/open5gs-upfd.service.d
@@ -64,8 +64,8 @@ EOF
 
 
 # CHANGE THE ADDRESS  PATCH THE CONFIGURATION OF SCP
-sudo sed -i 's/127\.0\.0\.200/127.0.1.10/' /etc/open5gs/scp.yaml
-sudo sed -i 's|http://127\.0\.0\.10:7777|http://127.0.0.200:7777|' /etc/open5gs/scp.yaml
+# sudo sed -i 's/127\.0\.0\.200/127.0.1.10/' /etc/open5gs/scp.yaml
+# sudo sed -i 's|http://127\.0\.0\.10:7777|http://127.0.0.200:7777|' /etc/open5gs/scp.yaml
 
 # COPY ALL SYSTEMD FILES
 sudo cp build/configs/systemd/*.service /etc/systemd/system/
